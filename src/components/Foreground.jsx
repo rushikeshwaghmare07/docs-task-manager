efa5479b318955304c60a9127db693d568cf3d2b
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-07-04 23:06:38 +0530

    Develop Card component to showcase file info

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Card from './Card';
 
 function Foreground() {
   return (
     <div className='fixed top-0 left-0 z-[3] w-full h-full'>
+      <Card/>
     </div>
   )
 }
