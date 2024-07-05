c78c05d9b2cdb04cd577c67f18d2f3e4d0b1e6c6
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-07-05 21:14:33 +0530

    Update foreground component

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import Card from './Card';
+import React, { useState } from "react";
+import Card from "./Card";
 
 function Foreground() {
+  const data = [
+    {
+      desc: "This is a post for...",
+      fileSize: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+  ];
   return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-      <Card/>
+    <div className="fixed top-0 left-0 z-[3] w-full h-full">
+      {data.map((item, index) => (
+        <Card data ={item}/>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Foreground;
