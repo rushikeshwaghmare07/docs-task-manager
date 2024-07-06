0cc3c00d707539db5c77fff15d9802a456dafdad
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-07-06 21:30:05 +0530

    Update cards dynamically

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import { IoClose } from "react-icons/io5";
 
 function Card({data}) {
   return (
-    <div className='relative w-60 h-80 rounded-[45px] bg-zinc-900/90 text-white p-8 py-10 overflow-hidden'>
+    <div className='relative flex-shrink-0 w-60 h-80 rounded-[45px] bg-zinc-900/90 text-white p-8 py-10 overflow-hidden'>
       <FaRegFileAlt/>
       <p className='text-sm leading-tight mt-5 font-semibold'>{data.desc}</p>
       <div className='footer absolute bottom-0 w-full left-0'>
@@ -15,8 +15,14 @@ function Card({data}) {
             {data.close ? <IoClose /> : <LuDownload size=".7em" color='#000' />}
           </span>
         </div>
-        <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
-          <h3 className='text-sm font-semibold'>Download Now</h3>
+        <div>
+          {
+            data.tag.isOpen && ( 
+              <div className={`tag w-full py-4 ${data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"} flex items-center justify-center`}>
+                <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
+              </div>
+            )
+          }
         </div>
       </div>
     </div>
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -9,9 +9,21 @@ function Foreground() {
       close: true,
       tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
     },
+    {
+      desc: "This is a post for...",
+      fileSize: ".9mb",
+      close: true,
+      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "blue" },
+    },
+    {
+      desc: "This is a post for...",
+      fileSize: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Upload", tagColor: "green" },
+    },
   ];
   return (
-    <div className="fixed top-0 left-0 z-[3] w-full h-full">
+    <div className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5">
       {data.map((item, index) => (
         <Card data ={item}/>
       ))}
