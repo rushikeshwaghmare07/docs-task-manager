545046750e3be91caac418a2862168e80767cd68
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-07-06 21:44:09 +0530

    Added drag animation on cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
 import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion"
 
-function Card({data}) {
+function Card({data, reference}) {
   return (
-    <div className='relative flex-shrink-0 w-60 h-80 rounded-[45px] bg-zinc-900/90 text-white p-8 py-10 overflow-hidden'>
+    <motion.div drag dragConstraints={reference} whileDrag={{scale: 1.2}} dragElastic={0.2} className='relative flex-shrink-0 w-60 h-80 rounded-[45px] bg-zinc-900/90 text-white p-8 py-10 overflow-hidden'>
       <FaRegFileAlt/>
       <p className='text-sm leading-tight mt-5 font-semibold'>{data.desc}</p>
       <div className='footer absolute bottom-0 w-full left-0'>
@@ -25,7 +26,7 @@ function Card({data}) {
           }
         </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Card from "./Card";
 
 function Foreground() {
+
+  const ref = useRef(null);
+
   const data = [
     {
       desc: "This is a post for...",
@@ -21,11 +24,11 @@ function Foreground() {
       close: true,
       tag: { isOpen: true, tagTitle: "Upload", tagColor: "green" },
     },
-  ];
+  ]; 
   return (
-    <div className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5">
+    <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5">
       {data.map((item, index) => (
-        <Card data ={item}/>
+        <Card data ={item} reference={ref} />
       ))}
     </div>
   );
